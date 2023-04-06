197241df947b1c1d87691efc0412cba218c98c62
Dhinesh T <dhinesh@dorustree.in>
2023-04-06 12:38:22 +0530

    Axios vs Fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+//AXIOS
+
+  useEffect(() => {
+    const loadPost = async () => {
+      setLoading(true);
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      setPosts(response.data);
+      setLoading(false);
+    };
+
+    //FETCH-API
+    // const loadPost = () => {
+    //   setLoading(true);
+    //   return fetch("https://jsonplaceholder.typicode.com/posts", {
+    //     method: "GET",
+    //   })
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //       setLoading(false);
+    //       setPosts(data);
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //       setLoading(false);
+    //     });
+    // };
+
+    loadPost();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Axios vs Fetch in React JS</h1>
+      {loading ? (
+        <h4>Loading ...</h4>
+      ) : (
+        posts.map((item) => <h5 key={item.id}>{item.title}</h5>)
+      )}
     </div>
   );
 }
